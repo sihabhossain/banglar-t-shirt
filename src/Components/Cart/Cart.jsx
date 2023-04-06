dbc882688bc8944eb20a35e110bc8b7844860789
Sihab Hossain <sihabhossain380@gmail.com>
2023-04-07 00:02:01 +0600

    ternary added

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,16 +1,39 @@
 import { key } from "localforage";
 import React from "react";
+import "./Cart.css";
 
-const Cart = ({ cart, HandleRemoveFromCart }) => {
+const Cart = ({ cart, handleRemoveFromCart }) => {
+  let messege;
+  if (cart.length === 0) {
+    messege = <p>Please add some products</p>;
+  } else {
+    messege = <p>you are poor now lmao</p>;
+  }
   return (
     <div>
-      <h2>Order Summary: {cart.length}</h2>
+      <h2 className={cart.length === 1 ? "blue" : "red"}>
+        Order Summary: {cart.length}
+      </h2>
+
+      <p className={`bold ${cart.length === 3 ? "yellow" : "purple"}`}>
+        something
+      </p>
+
+      {cart.length > 2 ? (
+        <span className="purple">Aro kino</span>
+      ) : (
+        <span>fokira</span>
+      )}
+      {messege}
+
       {cart.map((tshirt) => (
         <p key={tshirt._id}>
           {tshirt.name}{" "}
-          <button onClick={() => HandleRemoveFromCart(tshirt._id)}>x</button>
+          <button onClick={() => handleRemoveFromCart(tshirt._id)}>x</button>
         </p>
       ))}
+      {cart.length === 2 && <p>Double bonanza!!!</p>}
+      {cart.length === 3 || <h3>Tinta to hoilo na!!</h3>}
     </div>
   );
 };
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,27 +1,27 @@
 import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
-import Tshirt from "../Tshirt/Tshirt";
+
 import Cart from "../Cart/Cart";
 import "./Home.css";
 import toast from "react-hot-toast";
+import Tshirt from "../Tshirt/Tshirt";
 
 const Home = () => {
   const tshirts = useLoaderData();
   const [cart, setCart] = useState([]);
 
-  const HandleAddToCart = (tshirt) => {
-    const exists = cart.find((tshirt) => tshirt._id === tshirt._id);
-
+  const handleAddToCart = (tshirt) => {
+    const exists = cart.find((ts) => ts._id === tshirt._id);
     if (exists) {
-      toast("you have already added this one");
+      toast("You have already added this t-shirt");
     } else {
       const newCart = [...cart, tshirt];
       setCart(newCart);
     }
   };
 
-  const HandleRemoveFromCart = (id) => {
-    const remaining = cart.filter((tshirt) => tshirt._id !== id);
+  const handleRemoveFromCart = (id) => {
+    const remaining = cart.filter((ts) => ts._id !== id);
     setCart(remaining);
   };
 
@@ -32,12 +32,12 @@ const Home = () => {
           <Tshirt
             key={tshirt._id}
             tshirt={tshirt}
-            HandleAddToCart={HandleAddToCart}
+            handleAddToCart={handleAddToCart}
           ></Tshirt>
         ))}
       </div>
       <div className="cart-container">
-        <Cart cart={cart} HandleRemoveFromCart={HandleRemoveFromCart}></Cart>
+        <Cart cart={cart} handleRemoveFromCart={handleRemoveFromCart}></Cart>
       </div>
     </div>
   );
